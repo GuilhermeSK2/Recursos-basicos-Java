a6384db9e81f6d09da1353094a5f69bf0fa51216
Guilherme Freitas <139295562+GuilhermeSK2@users.noreply.github.com>
2024-10-03 08:58:54 -0300

    Add files via upload

---
+++ Constantes/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args){
+        //Variaveis imutáveis (Constantes)
+
+        final double PI = 3.14159;
+
+        //Como a variavel PI foi declarada como "final" ela se tornou uma constante, isso significa que não se pode mais alterar seu valor.
+        //Se tentarmos alterar seu valor como neste exemplo "PI = 1992;" será apresentado um erro de compilação.
+    }
+}
