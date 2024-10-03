5eecf8c6b4d66d384aa40a6f02026cbd3da1adc7
Guilherme Freitas <139295562+GuilhermeSK2@users.noreply.github.com>
2024-10-03 10:30:44 -0300

    Add files via upload

---
+++ TiposPrimitivos/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+
+    public static void main(String[] args){
+
+        //Tipos primitivos
+
+        /* boolean isLogged = true;
+        byte b = 'a';
+        char c = 'c';
+        short s = -129; //Igual ao nt só que com menos espaços
+        int i = 12345678; //Grande
+        long l = 1234232476576362L; // int só que com mais espaços
+        float f = 19.19f; //32bit menor que double
+        double d = 19.19; //64bit
+        */
+    }
+}
