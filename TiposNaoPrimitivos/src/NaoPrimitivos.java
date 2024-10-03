ceff699b93f38f8dfff672f7db84705ef15a5322
Guilherme Freitas <139295562+GuilhermeSK2@users.noreply.github.com>
2024-10-03 00:48:17 -0300

    Add files via upload

---
+++ TiposNaoPrimitivos/src/NaoPrimitivos.java
@@ -0,0 +1,8 @@
+public class NaoPrimitivos {
+    public static void main (String[] args) {
+        int x = 2; //Recomendado para trabalhar de forma simples somente para armazenar um simples número.
+        Integer y = 4; //Recomendado para trabalhar de forma mais complexa.
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
